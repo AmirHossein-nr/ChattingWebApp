d6703e0ce44473ed515b5e2867250bd8ce37ebed
Amirhossein-nr <arazlighi@gmail.com>
2022-02-15 20:53:17 +0330

    create user api (serverless)

---
+++ api/createUser.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const createUser = async (req, res) => {
+  const { userId, userName } = req.body;
+
+  axios
+    .post('https://api.chatengine.io/projects/people/',
+      { username: userName, secret: userId },
+      { headers: { 'Private-Key': "625d5f14-898e-4f07-9003-3e8a02a904bf" } },
+    )
+    .then(apiRes => {
+      res.json({
+        body: apiRes.data,
+        error: null,
+      });
+    })
+    .catch(() => {
+      res.json({
+        body: null,
+        error: 'There was an error creating the user',
+      });
+    });
+};
+
+export default createUser;
